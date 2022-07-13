34e64ad466d8f59853d8cc6338c4052ef7f1736b
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2022-07-13 09:15:44 +0100

    Changes due to deployment issues

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        scope: ['profile', 'email']
     }, async (accessToken, refreshToken, profile, done) => {
         const newUser = {
             googleId: profile.id,
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 
 // Login Auth with Google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 // Google Callback
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
